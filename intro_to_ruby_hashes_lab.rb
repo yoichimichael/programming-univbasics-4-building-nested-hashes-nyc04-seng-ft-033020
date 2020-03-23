f32331898a3ec279195fc6f103d24ab24f3637e9
Yoichi Nagano <yoichi.michael.nagano@gmail.com>
2020-03-23 06:31:42 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -30,7 +30,10 @@ def monopoly_with_third_tier
       three_pieces_owned: 100,
       four_pieces_owned: 200
     },
-    names: { :reading_railroad => {} }
+    names: { 
+      :reading_railroad => {},
+      :pennsylvania_railroad => {}
+    }
     } 
   }
 end

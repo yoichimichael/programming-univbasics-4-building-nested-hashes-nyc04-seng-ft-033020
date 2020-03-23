cfe87aefbe4f314b2ab625457d9c2683dc493009
Yoichi Nagano <yoichi.michael.nagano@gmail.com>
2020-03-23 06:29:23 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -24,7 +24,10 @@ def monopoly_with_third_tier
   # complexity.
   { :railroads => { 
     pieces: 4, 
-    rent_in_dollars: { one_piece_owned: 25 },
+    rent_in_dollars: { 
+      one_piece_owned: 25,
+      two_pieces_owned: 50
+    },
     names: {}
     } 
   }
